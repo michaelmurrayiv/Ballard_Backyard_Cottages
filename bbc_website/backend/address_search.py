c5907e8bfb79b68717f523100c58247a46cdfad8
Michael Murray <michael.murray.iv@gmail.com>
2024-08-16 07:44:47 -0700

    got all tabs to open in same window. Might still want to add dadudoctor tab as well?

--- bbc_website/backend/address_search.py
+++ bbc_website/backend/address_search.py
@@ -3,7 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
 
 def get_address():
     a = input("Enter the address of the property: ")
@@ -12,22 +18,23 @@ def get_address():
 def open_google_maps(address):
     encoded_address = address.replace(" ", "+")
     url = f"https://www.google.com/maps/search/?api=1&query={encoded_address}"
-    webbrowser.open(url)
+    # webbrowser.open(url)
+    driver.execute_script(f"window.open('{url}', '_blank');")
+
 
 def open_parcel_viewer(address):
-    chrome_options = Options() 
-    chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=chrome_options)
+
 
     driver.get("https://gismaps.kingcounty.gov/parcelviewer2/")
-    time.sleep(6)
 
-    search_box = driver.find_element(By.ID, "searchInput")
+    wait = WebDriverWait(driver, 10)
+    search_box = wait.until(EC.presence_of_element_located((By.ID, "searchInput")))
+
+   # search_box = driver.find_element(By.ID, "searchInput")
     search_box.send_keys(address)
     search_box.send_keys(Keys.RETURN)
-    time.sleep(4)
-    
-    parcel_element = driver.find_element(By.CSS_SELECTOR, "td.dojoxGridCell[idx='1']")
+    wait = WebDriverWait(driver, 10)
+    parcel_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "td.dojoxGridCell[idx='1']")))
     parcel_number = parcel_element.text
     print("Parcel Number:", parcel_number)
 
@@ -35,8 +42,9 @@ def open_parcel_viewer(address):
 
 def open_property_detail(parcel_number):
     url = f"https://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr={parcel_number}"
-    webbrowser.open(url)
-    
+    # webbrowser.open(url)
+    driver.execute_script(f"window.open('{url}', '_blank');")
+
 # MAIN
 
 def main():

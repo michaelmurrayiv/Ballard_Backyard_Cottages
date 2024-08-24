bbc84867fe26a78a32f5b3e72f50394366fbc996
Michael Murray <michael.murray.iv@gmail.com>
2024-08-24 16:15:15 -0700

    added aduniverse to script. Can't figure out how to select the search bar, copied address to clipboard instead.

--- bbc_website/backend/address_search.py
+++ bbc_website/backend/address_search.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import sys
+import pyperclip
 
 chrome_options = Options() 
 chrome_options.add_experimental_option("detach", True)
@@ -19,22 +20,17 @@ def get_address():
 def open_google_maps(address):
     encoded_address = address.replace(" ", "+")
     url = f"https://www.google.com/maps/search/?api=1&query={encoded_address}"
-    # webbrowser.open(url)
     driver.execute_script(f"window.open('{url}', '_blank');")
 
 
 def open_parcel_viewer(address):
-
-
     driver.get("https://gismaps.kingcounty.gov/parcelviewer2/")
-
     wait = WebDriverWait(driver, 10)
     search_box = wait.until(EC.presence_of_element_located((By.ID, "searchInput")))
-
-   # search_box = driver.find_element(By.ID, "searchInput")
     search_box.send_keys(address)
     search_box.send_keys(Keys.RETURN)
     wait = WebDriverWait(driver, 10)
+    
     parcel_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "td.dojoxGridCell[idx='1']")))
     parcel_number = parcel_element.text
     print("Parcel Number:", parcel_number)
@@ -43,13 +39,26 @@ def open_parcel_viewer(address):
 
 def open_property_detail(parcel_number):
     url = f"https://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr={parcel_number}"
-    # webbrowser.open(url)
     driver.execute_script(f"window.open('{url}', '_blank');")
 
+
+def open_aduniverse(address):
+    url = "https://aduniverse-seattlecitygis.hub.arcgis.com/pages/feasibility"
+    driver.execute_script(f"window.open('{url}', '_blank');")
+    driver.switch_to.window(driver.window_handles[-1])
+
+    wait = WebDriverWait(driver, 10)
+    search_box = EC.presence_of_element_located(By.CLASS_NAME, "esri-input esri-search__input")
+    
+
+    search_box.send_keys("test")
+    #search_box.send_keys(Keys.RETURN)
+    
 # MAIN
 
 def main():
     address = get_address()
+    pyperclip.copy(address)
     try:
         parcel = open_parcel_viewer(address)
     except:
@@ -57,5 +66,6 @@ def main():
         return
     open_google_maps(address)
     open_property_detail(parcel)
+    open_aduniverse(address)
 
 main()
